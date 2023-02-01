4dc1839068e8e66ecdfd1dde09597d5f4781d12f
Hisham <hisham.taha.work+0@gmail.com>
2023-02-01 21:46:34 +0200

    solution

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 var app = express();
+
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// General log middleware
+app.use((req, res, next) => {
+  const { ip, method, path, query, body, params, headers } = req;
+  console.log({ ip, method, path, query, body, params /* , headers */ });
+  next();
+});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +25,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+// File uploader API
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname: name, mimetype: type, size } = req.file;
+  res.json({ name, type, size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
